377935f48bd1a01cceb3439db7a3dc2a8fb156e7
Gayle Martin <hello.gmartin@gmail.com>
2024-03-10 14:30:18 -0500

    Create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'), // Serve files from the public directory
+    compress: true, // Enable gzip compression for everything served
+    port: 9000, // Specify a port to use for the dev server
+  },
+};
