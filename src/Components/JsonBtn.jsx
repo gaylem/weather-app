e20e71ffa1f83ccbd22e6382a8d5240bde1655ea
Gayle Martin <hello.gmartin@gmail.com>
2024-03-10 20:26:42 -0500

    Update JsonBtn to open in a new  tab

--- src/Components/JsonBtn.jsx
+++ src/Components/JsonBtn.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from 'react';
-import ReactJson from 'react-json-view';
+import React from 'react';
 
 const JsonBtn = ({ weatherData }) => {
   if (!weatherData) {
     return null; // Return null if weatherData is not available yet
   }
 
-  const [showJson, setShowJson] = useState(false);
-
-  const onToggle = () => {
-    setShowJson(!showJson);
+  const openJsonInNewTab = () => {
+    const jsonString = JSON.stringify(weatherData, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    window.open(url, '_blank');
+    URL.revokeObjectURL(url);
   };
 
   return (
     <div className='container mx-auto flex flex-col divide-solid p-10'>
-      <button onClick={onToggle} className='text-blue-500 underline hover:text-blue-700'>
-        View Raw JSON
+      <button onClick={openJsonInNewTab} className='text-blue-500 underline hover:text-blue-700'>
+        View Raw JSON in New Tab
       </button>
-      {showJson && (
-        <div className='json-modal'>
-          <ReactJson src={weatherData} theme='monokai' />
-        </div>
-      )}
     </div>
   );
 };
