b5342e16793f78f1c609bed6bce2ef430da80cd5
Gayle Martin <hello.gmartin@gmail.com>
2024-03-11 11:55:51 -0500

    Update button and flexbox styles

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -46,13 +46,13 @@ const SearchBar = () => {
               <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
             </svg>
           </div>
-          <input type='search' id='default-search' className='block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500' placeholder='Search for cities with comma-separated values' required value={city} onChange={e => setCity(e.target.value)} />
-          <button type='submit' className='text-white absolute right-2.5 bottom-2.5 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800'>
+          <input type='search' id='default-search' className='block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500' placeholder='Search for cities with comma-separated values' required value={city} onChange={e => setCity(e.target.value)} />
+          <button type='submit' className='text-white absolute right-2.5 bottom-2.5 bg-sky-600 hover:bg-cyan-500 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-cyan-500 dark:focus:ring-sky-800'>
             Search
           </button>
         </div>
       </form>
-      <div className='flex space-x-4'>
+      <div className='flex flex-wrap sm:flex-nowrap space-x-2'>
         {weatherData && <JsonBtn weatherData={weatherData} />}
         {weatherData && <ExcelBtn weatherData={weatherData} />}
       </div>
