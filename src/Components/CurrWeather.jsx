4f0dbcf49533bc946eb2e3952880ebbd416b418d
Gayle Martin <hello.gmartin@gmail.com>
2024-03-11 11:53:07 -0500

    Add PastWeather to CurrWeather and implement toggle

--- src/Components/CurrWeather.jsx
+++ src/Components/CurrWeather.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PastWeather from './PastWeather.jsx';
 
 function CurrWeather({ cityName, weatherData }) {
+  const [showPastWeather, setShowPastWeather] = useState(false);
+
   if (!weatherData) {
-    return null; // Return null if weatherData is not available yet
+    return null;
   }
 
   const { weather, coord, main } = weatherData.current_weather;
@@ -11,11 +14,15 @@ function CurrWeather({ cityName, weatherData }) {
   const iconId = weather[0].icon;
   const { lat, lon } = coord;
 
+  const handleViewPastWeather = () => {
+    setShowPastWeather(prevState => !prevState);
+  };
+
   return (
     <div className='container mx-auto p-10'>
-      <h1 className='text-sky-700'>{cityName} Current Weather</h1>
-      <div className='flex flex-wrap gap-4 mt-4 bg-gray-100'>
-        <div className='p-4 flex items-center flex-wrap'>
+      <h1 className='text-sky-700 text-3xl font-bold'>{cityName} Current Weather</h1>
+      <div className='flex flex-wrap gap-4 mt-4 bg-gray-100 rounded-lg'>
+        <div className='p-4 flex justify-evenly items-center flex-wrap w-full'>
           <img src={`http://openweathermap.org/img/w/${iconId}.png`} alt='Weather Icon' className='mr-4' />
           <div>
             <p>Temperature: {main.temp}</p>
@@ -29,8 +36,14 @@ function CurrWeather({ cityName, weatherData }) {
             <p>Latitude: {lat}</p>
             <p>Longitude: {lon}</p>
           </div>
+          <div className='mr-4'>
+            <button className='text-white bg-sky-600 hover:bg-cyan-500 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-cyan-500 dark:focus:ring-sky-800' onClick={handleViewPastWeather}>
+              {showPastWeather ? 'Hide Past Weather Data' : 'View Past Weather Data'}
+            </button>
+          </div>
         </div>
       </div>
+      {showPastWeather && <PastWeather cityName={cityName} weatherData={weatherData} />}
     </div>
   );
 }
