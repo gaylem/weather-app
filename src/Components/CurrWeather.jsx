a41071cdfa87d4f902e6da12f55d8aa4e0c85f7c
Gayle Martin <hello.gmartin@gmail.com>
2024-03-10 20:27:05 -0500

    Adjust grid

--- src/Components/CurrWeather.jsx
+++ src/Components/CurrWeather.jsx
@@ -12,21 +12,23 @@ function CurrWeather({ cityName, weatherData }) {
   const { lat, lon } = coord;
 
   return (
-    <div className='container mx-auto flex flex-col divide-solid p-10'>
+    <div className='container mx-auto p-10'>
       <h1 className='text-sky-700'>{cityName} Current Weather</h1>
-      <div className='flex items-center'>
-        <img src={`http://openweathermap.org/img/w/${iconId}.png`} alt='Weather Icon' className='mr-4' />
-        <div>
-          <p>Temperature: {main.temp}</p>
-          <p>Main Weather: {mainWeather}</p>
-          <p>Description: {description}</p>
-          <p>Feels Like: {main.feels_like}</p>
-          <p>Humidity: {main.humidity}</p>
-          <p>Pressure: {main.pressure}</p>
-          <p>Max Temperature: {main.temp_max}</p>
-          <p>Min Temperature: {main.temp_min}</p>
-          <p>Latitude: {lat}</p>
-          <p>Longitude: {lon}</p>
+      <div className='grid grid-cols-2 md:grid-cols-1 gap-4 mt-4'>
+        <div className='bg-gray-100 p-4 flex items-center flex-wrap'>
+          <img src={`http://openweathermap.org/img/w/${iconId}.png`} alt='Weather Icon' className='mr-4' />
+          <div className='grid grid-cols-2 gap-4 mt-4' style={{ flexWrap: 'wrap' }}>
+            <p>Temperature: {main.temp}</p>
+            <p>Max Temperature: {main.temp_max}</p>
+            <p>Min Temperature: {main.temp_min}</p>
+            <p>Main Weather: {mainWeather}</p>
+            <p>Description: {description}</p>
+            <p>Feels Like: {main.feels_like}</p>
+            <p>Humidity: {main.humidity}</p>
+            <p>Pressure: {main.pressure}</p>
+            <p>Latitude: {lat}</p>
+            <p>Longitude: {lon}</p>
+          </div>
         </div>
       </div>
     </div>
