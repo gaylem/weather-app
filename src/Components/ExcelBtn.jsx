6ca6eb233270d8261aa8de53feaa2563a9d81783
Gayle Martin <hello.gmartin@gmail.com>
2024-03-11 11:53:42 -0500

    Improve flattening function

--- src/Components/ExcelBtn.jsx
+++ src/Components/ExcelBtn.jsx
@@ -3,32 +3,45 @@ import * as XLSX from 'xlsx';
 
 const ExcelBtn = ({ weatherData }) => {
   const downloadExcel = () => {
-    // Flatten the nested structure and select relevant fields
-    const flattenedData = weatherData.map(cityData => ({
-      city: cityData.current_weather.name,
-      country: cityData.current_weather.sys.country,
-      temperature: cityData.current_weather.main.temp,
-      humidity: cityData.current_weather.main.humidity,
-      pressure: cityData.current_weather.main.pressure,
-      weatherDescription: cityData.current_weather.weather[0].description,
-      windSpeed: cityData.current_weather.wind.speed,
-    }));
+    // Flatten data
+    const flattenedData = weatherData.flatMap(cityData =>
+      cityData.historical_weather.map(weather => ({
+        city: cityData.current_weather.name,
+        country: cityData.current_weather.sys.country,
+        date: new Date(weather.data[0].dt * 1000).toLocaleDateString(),
+        temperature: weather.data[0].temp,
+        humidity: weather.data[0].humidity,
+        pressure: weather.data[0].pressure,
+        weatherDescription: weather.data[0].weather[0].description,
+        windSpeed: weather.data[0].wind_speed,
+        clouds: weather.data[0].clouds,
+        dewPoint: weather.data[0].dew_point,
+        sunrise: new Date(weather.data[0].sunrise * 1000).toLocaleString(),
+        sunset: new Date(weather.data[0].sunset * 1000).toLocaleString(),
+        visibility: weather.data[0].visibility,
+        uvi: weather.data[0].uvi,
+        windDeg: weather.data[0].wind_deg,
+        windGust: weather.data[0].wind_gust,
+        timezone: weather.timezone,
+        timezoneOffset: weather.timezone_offset,
+      })),
+    );
 
-    // Convert the flattened data to an Excel sheet
+    // Convert to Excel sheet
     const worksheet = XLSX.utils.json_to_sheet(flattenedData);
 
-    // Create a new workbook and add the worksheet to it
+    // Create a new workbook
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'WeatherData');
 
-    // Download the Excel file
+    // Download Excel file
     XLSX.writeFile(workbook, 'weather_data.xlsx');
   };
 
   return (
     <div className='container mx-auto flex flex-col divide-solid p-10'>
-      <button onClick={downloadExcel} className='text-blue-500 underline hover:text-blue-700'>
-        Download Excel
+      <button onClick={downloadExcel} className='text-white bg-sky-600 hover:bg-cyan-500 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-cyan-500 dark:focus:ring-sky-800'>
+        Download Past Data to Excel
       </button>
     </div>
   );
