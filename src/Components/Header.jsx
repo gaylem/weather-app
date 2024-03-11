79235b372ba7ae0dceb61f064c2ad2cff50ea0d3
Gayle Martin <hello.gmartin@gmail.com>
2024-03-11 11:54:21 -0500

    Update styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className='bg-sky-700 text-white py-4'>
+    <header className='bg-gradient-to-r from-sky-700 to-cyan-500 text-white py-4'>
       <div className='container mx-auto flex justify-center items-center'>
         <h1 className='text-2xl font-bold'>Weather App</h1>
       </div>
