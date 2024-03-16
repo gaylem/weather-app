5b9cd76bb023f39c30d889415e09407f83f9b200
Gayle Martin <hello.gmartin@gmail.com>
2024-03-16 18:32:25 -0500

    Update styles

--- src/Components/PastWeather.jsx
+++ src/Components/PastWeather.jsx
@@ -8,17 +8,17 @@ function PastWeather({ cityName, weatherData }) {
   }
 
   return (
-    <div className='container mx-auto p-4'>
+    <div className='p-4'>
       <hr />
       {/* //! First letter of cityName always capitalized */}
       <h2 className='text-sky-700 text-2xl sm:text-2xl font-bold p-5'>{cityName.charAt(0).toUpperCase() + cityName.slice(1)} Past Weather</h2>
-      <div className='flex flex-col gap-4 p-4'>
+      <div className='flex flex-wrap flex-col gap-4'>
         {weatherData.historical_weather.map((weather, index) => (
-          <div key={index} className='flex bg-white rounded-lg shadow-md p-3'>
-            <div>
-              <img src={`http://openweathermap.org/img/w/${weather.data[0].weather[0].icon}.png`} alt='Weather Icon' className='w-12 h-12 sm:w-16 sm:h-16 mr-3 sm:mr-4 float-left' />
+          <div key={index} className='flex flex-wrap bg-white rounded-lg shadow-md'>
+            <div className='p-5'>
+              <img src={`http://openweathermap.org/img/w/${weather.data[0].weather[0].icon}.png`} alt='Weather Icon' className='w-12 h-12 sm:w-16 sm:h-16 float-left' />
             </div>
-            <div>
+            <div className='p-5'>
               <h3 className='text-lg font-semibold'>Date: {new Date(weather.data[0].dt * 1000).toLocaleDateString()}</h3>
               <p>Temperature: {kelvinToFahrenheit(weather.data[0].temp)}°F</p>
               <p>Main Weather: {weather.data[0].weather[0].main}</p>
