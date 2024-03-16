6e825f5f5f64bb18be9ed8eb511841418c831f29
Gayle Martin <hello.gmartin@gmail.com>
2024-03-16 17:08:15 -0500

    Adjust styles

--- src/Components/JsonBtn.jsx
+++ src/Components/JsonBtn.jsx
@@ -23,7 +23,7 @@ const JsonBtn = ({ weatherData }) => {
   };
 
   return (
-    <div className='container mx-auto flex flex-col divide-solid p-10'>
+    <div className='container mx-auto flex flex-col divide-solid p-5'>
       <button onClick={openJsonInNewTab} className='text-white bg-sky-600 hover:bg-cyan-500 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-cyan-500 dark:focus:ring-sky-800'>
         View Raw JSON in New Tab
       </button>
